12803af237735b3f16117214ae5f35b7de03d6ac
Daniil <petrovdanil230@gmail.com~>
2025-12-25 18:42:16 +0300

    fix(security): del print message with password

--- backend/src/app/core/security.py
+++ backend/src/app/core/security.py
@@ -13,7 +13,6 @@ def verify_password(plain_password: str, hashed_password: str) -> bool:
     return pwd_context.verify(plain_password, hashed_password)
 
 def get_password_hash(password: str) -> str:
-    print('PASSWORD=',password)
     return pwd_context.hash(password)
 
 def create_access_token(user_id: str) -> str:
